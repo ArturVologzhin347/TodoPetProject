7f86e96b819840dabb4f366075ce8dce69956af6
ArturVologzhin347 <artsoftwareadm@gmail.com>
2022-06-15 04:57:34 +0500

    Initialize Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     id("com.android.application") version "7.2.0" apply false
     id("com.android.library") version "7.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+
+    id("com.diffplug.spotless")
     id("io.gitlab.arturbosch.detekt")
+
 }
 
 buildscript {
@@ -22,7 +25,33 @@ detekt {
     basePath = projectDir.path
 }
 
+spotless {
+    java {
+        target("**/*.java")
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    kotlin {
+        target("**/*.kt")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+
+    format("misc") {
+        target("**/*.gradle", "**/*.md", "**/.gitignore")
+        indentWithSpaces()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    format("xml") {
+        target("**/*.xml")
+        indentWithSpaces()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
